3d9f64ceacf8fdedcbf454542b0c84d6eded8b0b
zain <zih301@gmail.com>
2022-03-06 15:45:30 -0500

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="salat",
-    version="0.1.0",
+    version="0.2.0",
     description="Tool to calculate accurate salat (prayer) times",
     long_description=README,
     long_description_content_type="text/markdown",
